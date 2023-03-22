c80b6422fa293e2b245138224938391b4eecb8f2
VictorAlas92_portatil_Trabajo <ingvicalo1092@gmail.com>
2023-03-22 14:03:46 -0600

    81-Alert Screen -Disenno se agrega el disenno de la pantalla alerta y se agrega a apptheme el tema de los ElevationButton

--- lib/ui/screens/alert_screen.dart
+++ lib/ui/screens/alert_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
@@ -5,9 +6,31 @@ class AlertScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          // style: ElevatedButton.styleFrom(
+          //   backgroundColor: Colors.amber,
+          //   shape: const StadiumBorder(),
+          // ), //se cabia a app_theme.dart
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.amber,
+          ),
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Mostrar alerta',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.close),
       ),
     );
   }
--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -4,18 +4,29 @@ class AppTheme {
   static const Color primary = Colors.teal;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    // Color primario
-    primaryColor: Colors.indigo,
-    // appBar Theme
-    appBarTheme: const AppBarTheme(
-      color: primary,
-      elevation: 0,
-    ),
-    // TextButton Theme
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: primary),
-    ),
-  );
+      // Color primario
+      primaryColor: Colors.indigo,
+      // appBar Theme
+      appBarTheme: const AppBarTheme(
+        color: primary,
+        elevation: 0,
+      ),
+      // TextButton Theme
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: primary),
+      ),
+
+// floatingActionBotton
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: primary,
+        elevation: 0.2,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: primary,
+            shape: const StadiumBorder(),
+            elevation: 0.2),
+      ));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     // Color primario
