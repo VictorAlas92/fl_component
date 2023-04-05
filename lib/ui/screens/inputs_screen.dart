2eb118754712ab9797f1cbc61753b38aacd3c301
VictorAlas92_portatil_Trabajo <ingvicalo1092@gmail.com>
2023-04-05 09:50:25 -0600

    88 perzonalizacion de textformfields _ se explican y se usan varias propuedades de el widget textformfield

--- lib/ui/screens/inputs_screen.dart
+++ lib/ui/screens/inputs_screen.dart
@@ -23,6 +23,33 @@ class InputsScreen extends StatelessWidget {
                   onChanged: (value) {
                     print(value);
                   },
+                  validator: (value) {
+                    if (value == null) return 'este campo es requerido';
+                    return value.length < 3 ? 'minimo de 3 letras' : null;
+                  },
+                  //*autovalidateMode condiciona el momento en que inicia a ejectarse el Validator
+                  //* este tambien puede ser usado en la parte del formulario y el se encargara
+                  //*de revisar todos los formfields y condicionar sus validator
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                    hintText: 'name user',
+                    labelText: 'Nombre',
+                    helperText: 'Solo letras',
+                    // counterText: '3 caracteres',
+                    suffixIcon: Icon(Icons.group_outlined),
+                    // prefixIcon: Icon(Icons.group_rounded),
+                    icon: Icon(Icons.assignment_ind_outlined),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        // topLeft: Radius.circular(10),
+                        // bottomRight: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
                 )
               ],
             ),
