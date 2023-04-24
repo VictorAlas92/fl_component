bf124c3464c95cef198731dc559e0534a79b38c5
VictorAlas92_portatil_Trabajo <ingvicalo1092@gmail.com>
2023-04-24 13:49:41 -0600

    se hace practica donde se ve el uso de 2 tipos de checkbox y 2 switches, los sencillos y los listtile los listtile nos permiten agregar mas cosas como titulos y cambios en los colores usando temas

--- lib/ui/screens/slider_screen.dart
+++ lib/ui/screens/slider_screen.dart
@@ -11,36 +11,68 @@ class sliderScreen extends StatefulWidget {
 
 class _sliderScreenState extends State<sliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnable = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sliders && Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-                min: 50,
-                max: 400,
-                activeColor: AppTheme.primary,
-                divisions: 15,
-                value: _sliderValue,
-                onChanged: (value) {
-                  _sliderValue = value;
-                  setState(() {});
-                }),
-            Image(
-              image: NetworkImage(
-                  'https://imagen.research.google/main_gallery_images/a-photo-of-a-corgi-dog-riding-a-bike-in-times-square.jpg'),
-              width: _sliderValue,
-              fit: BoxFit.contain,
+      body: Column(
+        children: [
+          Slider.adaptive(
+              min: 50,
+              max: 400,
+              activeColor: AppTheme.primary,
+              divisions: 15,
+              value: _sliderValue,
+              onChanged: _sliderEnable
+                  ? (value) {
+                      _sliderValue = value;
+                      setState(() {});
+                    }
+                  : null),
+          // Checkbox(
+          //     value: _sliderEnable,
+          //     onChanged: (value) {
+          //       _sliderEnable = value ?? true;
+          //       setState(() {});
+          //     }),
+
+          // Switch(
+          //     value: _sliderEnable,
+          //     onChanged: (value) => setState(() {
+          //           _sliderEnable = value;
+          //         })),
+
+          CheckboxListTile(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _sliderEnable,
+              onChanged: (value) => setState(() {
+                    _sliderEnable = value ?? true;
+                  })),
+          SwitchListTile.adaptive(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _sliderEnable,
+              onChanged: (value) => setState(() {
+                    _sliderEnable = value;
+                  })),
+
+          const AboutListTile(),
+
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                  image: const NetworkImage(
+                      'https://imagen.research.google/main_gallery_images/a-photo-of-a-corgi-dog-riding-a-bike-in-times-square.jpg'),
+                  width: _sliderValue,
+                  fit: BoxFit.contain),
             ),
-            const SizedBox(
-              height: 50,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
